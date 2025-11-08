7402366bb14312e607339f364838abcee8c875e1
otogelashvili <otarbasilevsi@gmail.com>
2025-11-08 22:50:46 +0400

    feat: add data fetching service + interfaces

---
+++ src/app/services/data.services.ts
@@ -0,0 +1,33 @@
+import { inject, Injectable } from '@angular/core';
+import { User } from '../interfaces/user.model';
+import { HttpClient } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+import { Post } from '../interfaces/post.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  private httpClient = inject(HttpClient);
+
+  getUsers() {
+    return this.fetchData<User[]>(
+      'https://jsonplaceholder.typicode.com/users',
+      'Failed to fetch users'
+    );
+  }
+  getPosts() {
+    return this.fetchData<Post[]>(
+      'https://jsonplaceholder.typicode.com/posts',
+      'Failed to fetch posts'
+    );
+  }
+  private fetchData<T>(url: string, errorMsg: string) {
+    return this.httpClient.get<T>(url).pipe(
+      catchError((error) => {
+        console.error(errorMsg, error);
+        return throwError(() => new Error(errorMsg));
+      })
+    );
+  }
+}
