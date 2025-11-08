4265a7df0b8cf084de985166ef43319029da4816
otogelashvili <otarbasilevsi@gmail.com>
2025-11-08 22:52:56 +0400

    feat: add users page

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,44 @@
+import { Component, signal } from '@angular/core';
+import { DataService } from '../services/data.services';
+import { User } from '../interfaces/user.model';
+import { NgForOf } from '@angular/common';
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css'],
+  imports: [NgForOf],
+})
+export class UsersComponent {
+  users = signal<User[]>([]);
+  isFetching = signal(true);
+  error = signal('');
+
+  constructor(private dataService: DataService) {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.isFetching.set(true);
+    this.error.set('');
+
+    this.dataService.getUsers().subscribe({
+      next: (data) => {
+        this.users.set(
+          data.map((user) => {
+            return {
+              ...user,
+              firstName: user.name.split(' ')[0],
+              lastName: user.name.split(' ')[1],
+            };
+          })
+        );
+      },
+      error: (err) => {
+        console.log(err.message);
+        this.error.set(err.message);
+        this.isFetching.set(false);
+      },
+      complete: () => this.isFetching.set(false),
+    });
+  }
+}
